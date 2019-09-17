c8de38e28e1b4ab4aa1f3e937917d565b1a9cd9b
byllgrim <robin.a.t.pedersen@gmail.com>
2019-09-17 08:55:39 +0200

    simulate and display a control algorithm in 'runTests'

--- steering-algorithm/runTests.m
+++ steering-algorithm/runTests.m
@@ -1,2 +1,18 @@
+# This file generates input stimuli,
+# and applies it to each control algorithm,
+# and displays it in terminal window
+
+# TODO repeat this for all control_* algorithms
 stimuli = genInputStimuli(8);
-control_1d(stimuli);
+wheels = control_1d(stimuli);
+
+# TODO wrap the following code into a function
+printf("   IX    IY      OL    OR\n");
+for i = 1:length(wheels)
+    ix = stimuli(i, 1);
+    iy = stimuli(i, 2);
+    ol = wheels(i, 1);
+    or = wheels(i, 2);
+    printf("% .2f % .2f ⇒ % .2f % .2f\n",
+           ix, iy, ol, or);
+endfor
